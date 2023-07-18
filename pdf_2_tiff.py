40db0c392855e57b226d7bdbd3f265d8b4a00bba
manuel.alejandro <manuel.alejandro@oliveiratrust.com.br>
2023-07-18 14:07:24 -0300

    .

--- pdf_2_tiff.py
+++ pdf_2_tiff.py
@@ -8,6 +8,7 @@ import time
 import zipfile
 import cv2
 import numpy as np
+import pika
 from PIL import Image
 
 #from watchdog.events import FileSystemEventHandler
@@ -18,7 +19,7 @@ from PIL import Image
 #         arquivo_pdf = open(event.src_path, 'rb').read()
 #         threading.Thread(target=pdf_to_images, args=(arquivo_pdf,)).start()
 
-def pdf_to_images(arquivo_pdf):
+def pdf_to_images(arquivo_pdf, channel):
     folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',arquivo_pdf))
 
     file_name = arquivo_pdf.split('/')[-1]
@@ -34,32 +35,26 @@ def pdf_to_images(arquivo_pdf):
         tiff_path = os.path.join(f'output/{file_name}', f'{file_name}_page_{i+1}.tiff')
         cv2.imwrite(tiff_path, image)
 
+    channel.basic_publish(exchange='', routing_key='ocr_queue', body=file_name)
+    return file_name
 
-def get_all_files_from_zip(zip_file):
-    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-        i =0
-        k = len(zip_ref.namelist())
-        for file in zip_ref.namelist():
-            if i == 3:
-                break
-            i += 1
-            if file.endswith('.pdf'):
-                zip_ref.extract(file, 'temp/')
-                arquivo_pdf = open(f'temp/{file}', 'rb').read()
-                pdf_to_images(arquivo_pdf, file.split('.')[0], i, k)
-                os.remove(f'temp/{file}')
-                
 
 class Pdf2Tiff(Resource):
 
     def get(self):
+        conection_parameters =  pika.ConnectionParameters(host='localhost')
+        connection = pika.BlockingConnection(conection_parameters)
+
+        channel = connection.channel()
+        channel.queue_declare(queue='ocr_queue')
+
         s = time.time()
         path = request.form.get('path')
         # arquivo_zip = request.files['file']
         # if not arquivo_zip:
         #     return {'message': 'arquivo não subido'}, 400
 
-        pdf_to_images(path)
+        file = pdf_to_images(path, channel)
         t = time.time() - s
         
-        return {'message': 'convertindo pdfs para tiff', 'tempo de processamento de 1 contrato': t}, 200
+        return {'message': 'convertindo pdfs para tiff', 'contrato': f'{file}', 'tempo de processamento':t}, 200
